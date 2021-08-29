446bf655d841e41c45f90e8edc563f86729b5668
hkloudou <hkloudou@gmail.com>
2021-08-29 14:20:06 +0800

    fx

--- lib/src/deviceinfo.dart
+++ lib/src/deviceinfo.dart
@@ -25,31 +25,31 @@ Future<void> initDeviceID() async {
   }
   print("corex:_deviceID:$_deviceID");
   if (_deviceID == "") {
-    try {
-      print("corex:_deviceID hand");
-      var hand = await SharedPreferences.getInstance();
-      var key = packageInfo.packageName + "/deviceID";
-      print("corex:_deviceID key:$key");
-      String value = hand.getString(key) ?? "";
-      if (value.isEmpty) {
-        value = Uuid().v4();
-      }
-      _deviceID = value;
-      print("corex:_deviceID $_deviceID");
-      await hand.setString(key, value);
-    } catch (err) {}
-    // var hand = await SharedPreferences.getInstance();
-    // // tmp.get
-    // return SharedPreferences.getInstance().then((hand) {
+    // try {
     //   print("corex:_deviceID hand");
+    //   var hand = await SharedPreferences.getInstance();
     //   var key = packageInfo.packageName + "/deviceID";
+    //   print("corex:_deviceID key:$key");
     //   String value = hand.getString(key) ?? "";
     //   if (value.isEmpty) {
     //     value = Uuid().v4();
     //   }
     //   _deviceID = value;
-    //   return hand.setString(key, value);
-    // });
+    //   print("corex:_deviceID $_deviceID");
+    //   await hand.setString(key, value);
+    // } catch (err) {}
+    // var hand = await SharedPreferences.getInstance();
+    // // tmp.get
+    return SharedPreferences.getInstance().then((hand) {
+      print("corex:_deviceID hand");
+      var key = packageInfo.packageName + "/deviceID";
+      String value = hand.getString(key) ?? "";
+      if (value.isEmpty) {
+        value = Uuid().v4();
+      }
+      _deviceID = value;
+      return hand.setString(key, value);
+    });
   }
   return Future.value();
 }
