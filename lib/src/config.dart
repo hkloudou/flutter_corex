e0a894362639c58336b2f36a23568545ec602bcb
hkloudou <hkloudou@gmail.com>
2021-08-31 13:16:16 +0800

    fx

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -44,20 +44,20 @@ class EColor {
   static Color info1 = Color.fromRGBO(233, 233, 235, 255);
   static Color info2 = Color.fromRGBO(244, 244, 245, 255);
 
-  static const int _orangePrimaryValue = 0xFFf5bb05;
-  static const MaterialColor myorange = MaterialColor(
-    _orangePrimaryValue,
-    <int, Color>{
-      50: Color(0xFFFFF3E0),
-      100: Color(0xFFFFE0B2),
-      200: Color(0xFFFFCC80),
-      300: Color(0xFFFFB74D),
-      400: Color(0xFFFFA726),
-      500: Color(_orangePrimaryValue),
-      600: Color(0xFFFB8C00),
-      700: Color(0xFFF57C00),
-      800: Color(0xFFEF6C00),
-      900: Color(0xFFE65100),
-    },
-  );
+  // static const int _orangePrimaryValue = 0xFFf5bb05;
+  // static const MaterialColor myorange = MaterialColor(
+  //   _orangePrimaryValue,
+  //   <int, Color>{
+  //     50: Color(0xFFFFF3E0),
+  //     100: Color(0xFFFFE0B2),
+  //     200: Color(0xFFFFCC80),
+  //     300: Color(0xFFFFB74D),
+  //     400: Color(0xFFFFA726),
+  //     500: Color(_orangePrimaryValue),
+  //     600: Color(0xFFFB8C00),
+  //     700: Color(0xFFF57C00),
+  //     800: Color(0xFFEF6C00),
+  //     900: Color(0xFFE65100),
+  //   },
+  // );
 }
